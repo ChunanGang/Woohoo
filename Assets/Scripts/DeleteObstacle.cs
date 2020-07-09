011eedf4488ac40f3f6d5685f2a4326e33a98875
chu Oscar <chh179@ucsd.edu>
2020-07-08 18:05:29 -0700

    auto generate obstacle

--- Assets/Scripts/DeleteObstacle.cs
+++ Assets/Scripts/DeleteObstacle.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 
 public class DeleteObstacle : MonoBehaviour
 {
-    public float boundaryLeft = -10;
+    public float boundaryLeft = -7;
+    public GameManager gameManager;
+    private bool isLastObstacleInSeries = false; // whether this obstacle is the last in its series
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 
     // Update is called once per frame
@@ -17,7 +19,17 @@ public class DeleteObstacle : MonoBehaviour
     {
         if (transform.position.x < boundaryLeft)
         {
+            // call the game manager to spawn a new obstalce series, when the last obstacle 
+            //  in the previosu series is deleted, and when game is not over
+            if (GetComponent<DeleteObstacle>().isLastObstacleInSeries && !gameManager.gameOver)
+                gameManager.SpawnObstacleSeries();
             Object.Destroy(gameObject);
         }
+    }
+
+    // this function set the object as the last obstacle
+    public void setAsLastObstacle()
+    {
+        GetComponent<DeleteObstacle>().isLastObstacleInSeries = true;
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,8 +9,9 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     // Obstacles that move hirizontally
-    public GameObject cubeHorizontal;
+    public GameObject carHorizontal;
     public GameObject birdHorizontal;
+
     // public GameObject carHorizontal;
 
     public bool gameOver = false;
@@ -18,23 +19,82 @@ public class GameManager : MonoBehaviour
     // used to control the auto-spwaning of obstacles
     private float startDelay = 2.0f;
     private float interval = 1.5f;
+
+    private static float ProbDec = .1f; // used to generate obstacle series  
+    private static float obstacleDistance = 10.0f; // how far apart each obstacle from each other in one series
+   
     
     void Start()
     {
-        InvokeRepeating("SpawnObstacle", startDelay, interval);   
+        //InvokeRepeating("SpawnObstacle", startDelay, interval);
+        Invoke("SpawnObstacleSeries", 2);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (gameOver)
-            CancelInvoke();
+        
     }
 
-    // function that generate obstalces
-    void SpawnObstacle()
+    // function that generate obstalces series in the scene 
+    public void SpawnObstacleSeries()
     {
-        Instantiate(cubeHorizontal, cubeHorizontal.transform.position, cubeHorizontal.transform.rotation);
+        GameObject lastObstacle = null; // record the last created obstalce
+        GameObject creatingObstacle; // the curring creating object
+        float distance = 0.0f;
+
+        // generate obstacle series
+        string obstacleSeries = GenerateObstacleSeries("", 1);
+        foreach (char obstacle in obstacleSeries)
+        {
+            // get obstalce type
+            if (obstacle == '1')
+                creatingObstacle = carHorizontal;
+            else
+                creatingObstacle = birdHorizontal;
+            // create in the scnee
+            Vector3 createPosistion = new Vector3(creatingObstacle.transform.position.x + distance,
+                       creatingObstacle.transform.position.y,
+                       creatingObstacle.transform.position.z);
+            lastObstacle = Instantiate(creatingObstacle, createPosistion, creatingObstacle.transform.rotation);
+            // move distance
+            distance += obstacleDistance;
+        }
+
+        // mark the last created obstalce in the series
+        lastObstacle.GetComponent<DeleteObstacle>().setAsLastObstacle();
+    }
+
+    // functions that generate a series of ostacles that will follow the prevObstacle.
+    // Obstacle notation: "1" = carHorizontal; "2" = birdHorizontal
+    // prevObstacle: the previous one obstacle generated
+    // prob: probability that the next ostacle will get generated. 
+    //      function will return when prob <= 0
+    string GenerateObstacleSeries(string prevObstacle, float prob)
+    {
+        // check for stoping 
+        if (Random.Range(0.0001f, 1.0f) > prob)
+            return "";
+
+        // the next ostacle will get generated
+        int nextObstacleInt = Random.Range(1, 3); // random int between [1,2]
+        string nextObstacle = nextObstacleInt.ToString();
+
+        // there can be at most two continuous car (cannot be 3)
+        if ( prevObstacle == "1" && nextObstacle == "1")
+        {
+            // not next obstacle (stop atfer two conti-cars)
+            if (Random.Range(0.0001f, 1.0f) > prob-ProbDec)
+                return "1";
+            // have next obstacle (must be bird)
+            else
+                return "12" + GenerateObstacleSeries(nextObstacle, prob - 2*ProbDec);
+        }
+
+        // otherwise keep generating
+        else
+            return nextObstacle + GenerateObstacleSeries(nextObstacle, prob - ProbDec);
+      
     }
 
 }
